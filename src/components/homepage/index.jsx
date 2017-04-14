ea924ede6cf46748e5e986a90136f1bb16234144
hxm <dgdkhxm@163.com>
2017-04-14 11:05:02 +0800

    组件分离

---
+++ src/components/homepage/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import HomePageHeader from './HomePageHeader'
+import HomePageBody from './HomePageBody'
+import HomePageFooter from './HomePageFooter'
+import Alert from '../Alert'
+
+const HomePageComponent = ({teams, isShowForm, handleFormShow, handleDetailClick, handleFormSubmit, handleFormCancel}) => {
+  return (
+    <div className="container">
+      <div className="body-container">
+        <HomePageHeader />
+        <HomePageBody
+          teams={teams}
+          handleFormShow={handleFormShow}
+          handleDetailClick={handleDetailClick}
+        />
+        <HomePageFooter />
+      </div>
+      {isShowForm ? (
+        <Alert
+          handleSubmit={handleFormSubmit}
+          handleCancle={handleFormCancel}
+        />
+    ) : null}
+    </div>
+  )
+}
+
+HomePageComponent.propTypes = {
+  isShowForm: PropTypes.bool.isRequired,
+  handleFormShow: PropTypes.func.isRequired,
+  handleFormSubmit: PropTypes.func.isRequired,
+  handleFormCancel: PropTypes.func.isRequired
+}
+
+export default HomePageComponent
